ae87488a42e0ed97354a13d152782995a2ffaf1d
savormix <savormix@users.noreply.github.com>
2021-09-02 19:16:17 +0300

    Initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # msfs-h145-continuous-activator
-Run this Python script and have the HPG Airbus H145 activated without needing to manually run Activator.exe every 2 hours. If for some reason H145 insists on activation, use the "Toggle alternate static" hotkey to force-read the up-to-date license file (default: ALT+S).
+Run this Python script to have the HPG Airbus H145 activated without needing to manually run ActivatorApp.exe every 2 hours.
+
+In the event where you get the activation annunciator, hit ALT+S (Toggle alternate static) to force H145 to read the license file.
+
+You do not need to provide the activation key to this application - it will read it from the activation directory. As such, you **must** have used the ActivatorApp.exe (successfully) at least once (the file `hpg-airbus-h145/HPGH145/keycode.txt` already exists).
+
+## Quickstart
+
+### Preparations
+
+- Install [Python 3](https://www.python.org/downloads/), pick whichever is the latest (_3.5 is the oldest supported_).
+  - If unsure, Choose "system" install (for all users).
+  - Alternatively, [get from Microsoft store](https://www.microsoft.com/en-us/search?q=python).
+- `python -m pip -q -q -q --no-input --disable-pip-version-check --no-python-version-warning install requests`
+
+### Running the script
+
+- `python [path to this directory]/h145.py`
+
+## Troubleshooting
+
+- ### H145 activation directory: "&lt;UNKNOWN&gt;"
+
+`python [path to this directory]/h145.py --package [path to H145 directory (with ActivatorApp.exe)]`
+
+- ### Activation annunciator does not go away
+
+Please make sure this is your first flight after launching MSFS.
+Under certain conditions, the Early Release H145 will not read the license file - activation via ActivatorApp.exe will not work, Toggle alternate static (simconnect or hotkey) will also do nothing. The only resolution is to restart MSFS.
+
+
---
+++ h145.py
@@ -0,0 +1,96 @@
+import json
+import os
+import os.path
+import sys
+import time
+
+from argparse import ArgumentParser
+from base64 import standard_b64decode
+from datetime import datetime
+
+from requests import Session
+from requests.exceptions import RequestException, Timeout
+
+CHECK_INTERVAL: float = 300
+TIMEOUT_COOLDOWN: float = 5
+
+
+def _get_expiry(encoded_license: str) -> float:
+    meta_start = encoded_license.find('.') + 1
+    meta = json.loads(standard_b64decode(encoded_license[meta_start:encoded_license.find('.', meta_start)]))
+    return meta['exp'] / 1000
+
+
+def _format_expiry(timestamp: float) -> str:
+    return datetime.fromtimestamp(timestamp).isoformat()
+
+
+def _main() -> None:
+    parser = ArgumentParser(description='Activates HPG H145 and keeps activating it once it is about to expire.')
+    parser.add_argument('--package', metavar='dir', help='Path to the directory with Activator.exe')
+    cmd_args = parser.parse_args()
+    activation_dir = None
+    if cmd_args.package is None:
+        opt_file_path = os.path.join(os.getenv('APPDATA'), 'Microsoft Flight Simulator', 'UserCfg.opt')
+        with open(opt_file_path, 'rt', encoding='utf-8') as opt_file:
+            for line in opt_file:
+                line = line.strip()
+                if not line.startswith('InstalledPackagesPath'):
+                    continue
+                path_start = line.find('"') + 1
+                activation_dir = os.path.join(line[path_start:line.rfind('"')], 'hpg-airbus-h145', 'HPGH145')
+    else:
+        activation_dir = os.path.join(cmd_args.package, 'HPGH145')
+
+    print('H145 activation directory: "{}"'.format(activation_dir or '<UNKNOWN>'))
+
+    if activation_dir is None:
+        parser.print_usage()
+        sys.exit(1)
+
+    try:
+        with open(os.path.join(activation_dir, 'keycode.txt'), 'rt', encoding='us-ascii') as key_file:
+            key = key_file.read().strip()
+        current_expiry = 0
+    except FileNotFoundError:
+        print('License key file missing. Please activate once using Activator.exe')
+        sys.exit(1)
+    try:
+        with open(os.path.join(activation_dir, 'license.txt'), 'rt', encoding='us-ascii') as license_file:
+            current_expiry = _get_expiry((license_file.read()))
+        print('H145 activation expires on {} (local time)'.format(_format_expiry(current_expiry)))
+    except FileNotFoundError:
+        print('H145 activation expiry unknown')
+
+    with Session() as session:
+        session.params = {
+            'key': key
+        }
+        while True:
+            now = time.time()
+            if now < current_expiry - CHECK_INTERVAL:
+                time.sleep(CHECK_INTERVAL)
+                continue
+            try:
+                response = session.get('https://hpg-online-software-service.azurewebsites.net/api/Activate')
+                if response.status_code == 400:
+                    print('Not a valid license key: "{}"'.format(key))
+                    sys.exit(1)
+                if response.status_code != 200:
+                    print('Unknown activation response: {}'.format(response.status_code))
+                    raise Timeout()
+                license_content = response.text
+                with open(os.path.join(activation_dir, 'license.txt'), 'wt', encoding='us-ascii') as license_file:
+                    license_file.write(license_content)
+                current_expiry = _get_expiry(license_content)
+                print('H145 has been activated, will expire on {} (local time)'.format(_format_expiry(current_expiry)))
+            except Timeout:
+                time.sleep(TIMEOUT_COOLDOWN)
+                continue
+            except RequestException as e:
+                print(e)
+            time.sleep(CHECK_INTERVAL)
+
+
+if __name__ == '__main__':
+    _main()
